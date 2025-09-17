c245af22c88003a714a875316f8159d2b7867aec
ericthecurious <hello@ericthecurious.com>
2025-09-16 23:47:58 -0700

    patch: add missing export for fake

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ export * from './modules/ProtocolAnalyticsRunner'
 export { default as TactileStimulusController } from './modules/TactileStimulusController'
 export * from './modules/TactileStimulusController'
 
+export { default as FakeStimulusController } from './testDoubles/FakeStimulusController'
+export * from './testDoubles/FakeStimulusController'
+
 export { default as P001 } from './modules/protocols/P001'
 export * from './modules/protocols/P001'
 
