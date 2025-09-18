eaa3535bcc23cb0d4618748dd7bbf8e6dd8797c0
ericthecurious <hello@ericthecurious.com>
2025-09-18 00:03:33 -0700

    patch: add missing test

--- src/__tests__/modules/protocols/P002.test.ts
+++ src/__tests__/modules/protocols/P002.test.ts
@@ -2,6 +2,7 @@ import { assert, test } from '@sprucelabs/test-utils'
 import P002 from '../../../modules/protocols/P002'
 import { ProtocolRunner } from '../../../types'
 import AbstractPackageTest from '../../AbstractPackageTest'
+import AbstractProtocolRunner from '../../../modules/protocols/AbstractProtocolRunner'
 
 export default class P002Test extends AbstractPackageTest {
 	private static instance: ProtocolRunner
@@ -19,6 +20,10 @@ export default class P002Test extends AbstractPackageTest {
 		assert.isTruthy(this.instance, 'Should create an instance!')
 	}
 
+	@test()
+	protected static async extendsAbstractProtocolRunner() {
+		assert.isTrue(this.instance instanceof AbstractProtocolRunner, 'Should extend AbstractProtocolRunner!')
+	}
 	
 	protected static async P002() {
 		return P002.Create()
