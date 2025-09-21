e9909fd6be6b7dff913664ac085e47accb8c182d
ericthecurious <hello@ericthecurious.com>
2025-09-20 23:59:52 -0700

    patch: add missing import and organize

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 // ProtocolRunner
 
-export { default as ProtocolAnalyticsRunner } from './modules/ProtocolAnalyticsRunner'
-export * from './modules/ProtocolAnalyticsRunner'
+export { default as AbstractProtocolRunner } from './modules/protocols/AbstractProtocolRunner'
+export * from './modules/protocols/AbstractProtocolRunner'
+
+export { default as P001 } from './modules/protocols/P001'
+export * from './modules/protocols/P001'
+
+export { default as P002 } from './modules/protocols/P002'
+export * from './modules/protocols/P002'
 
 export { default as DummyProtocolRunner } from './testDoubles/ProtocolRunner/DummyProtocolRunner'
 export * from './testDoubles/ProtocolRunner/DummyProtocolRunner'
+
+// AnalyticsRunner
+
+export { default as ProtocolAnalyticsRunner } from './modules/ProtocolAnalyticsRunner'
+export * from './modules/ProtocolAnalyticsRunner'
+
+// StimulusController
 
 export { default as TactileStimulusController } from './modules/TactileStimulusController'
 export * from './modules/TactileStimulusController'
@@ -12,11 +25,7 @@ export * from './modules/TactileStimulusController'
 export { default as FakeStimulusController } from './testDoubles/StimulusController/FakeStimulusController'
 export * from './testDoubles/StimulusController/FakeStimulusController'
 
-export { default as P001 } from './modules/protocols/P001'
-export * from './modules/protocols/P001'
-
-export { default as P002 } from './modules/protocols/P002'
-export * from './modules/protocols/P002'
+// say.speak
 
 export { default as fakeSpeak } from './testDoubles/say/fakeSpeak'
 export * from './testDoubles/say/fakeSpeak'
